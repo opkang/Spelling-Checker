4c245a19117e4ff3fce351d28203a1564edcdbb0
Cheonny <cheon100215@gmail.com>
2024-01-22 17:11:17 +0800

    GUI blackpink and replacing candidate words

--- testing2.py
+++ testing2.py
@@ -34,68 +34,84 @@ class App(customtkinter.CTk):
         self.textbox.grid(row=0, column=0, sticky="nsew", padx=20, pady=50)
         self.textbox.insert("0.0", "")
         self.textbox.bind("<KeyRelease>",self.check)
-        # If want to add new binding events, put add='+', if not it will replaced by it
-        # self.textbox.bind('<Double-1>',self.double_click, add='+') 
-        
-        self.lableCount = customtkinter.CTkLabel(self,text="Word Count: 0")
-        self.lableCount.grid(row=2, column=0)  
-        
-        self.button = customtkinter.CTkButton(self, text="Show Candidates", command=self.show_candidate_event, width=35, height=20)  
-        self.button.grid(row=1, column=0)
 
-        # Create a themed style
-        self.style = ttk.Style()
+        self.lableCount = customtkinter.CTkLabel(self,text="Word Count: 0")
+        self.lableCount.grid(row=2, column=0,pady=10)  
+        
+        self.button = customtkinter.CTkButton(self, text="Show Candidates",
+                                            command=self.show_candidate_event, 
+                                            width=35, 
+                                            height=20,
+                                            border_width=10,
+                                            border_color="hotpink3",
+                                            fg_color="hotpink3",
+                                            hover_color="hotpink3")  
+        self.button.grid(row=1, column=0,padx=0,pady=0)
 
         # Configure the style for the Listbox
-        self.style.configure("DarkListbox.TListbox", background="#2E2E2E", foreground="white", selectbackground="white", selectforeground="white")
-        self.listbox = tk.Listbox(self, selectmode=tk.SINGLE,background="#2E2E2E",foreground="white")
-        self.listbox.grid(row=0, column=1, padx=0, pady=0, sticky="nsew")
+        self.style = ttk.Style()
+        self.listbox = tk.Listbox(self, 
+                                  selectmode=tk.SINGLE,
+                                  activestyle="none",
+                                  selectbackground="pink",
+                                  selectforeground="black",
+                                  background="#2E2E2E",
+                                  foreground="white",
+                                  font=("Helvetica", 12),
+                                  borderwidth=0, 
+                                  highlightthickness=20,
+                                  highlightbackground="#2E2E2E",
+                                  highlightcolor="#2E2E2E")
+        self.listbox.grid(row=0, column=1,rowspan=3, padx=20, pady=20, sticky="nsew")
+
 
     def select_candidate_event(self,t):
         selected_index = self.listbox.curselection()
         if selected_index:
-            selected_text = self.listbox.get(selected_index)
-            print(f"Listbox item clicked: {selected_text}")
+            selected_candidate = self.listbox.get(selected_index)
+            candidate_without_distance = re.sub(r'\(Edit Distance: \d+\)', '', selected_candidate)
+            self.textbox.delete(self.selection_indices[0], self.selection_indices[1])
+            self.textbox.insert(self.selection_indices[0], candidate_without_distance.strip())
+            self.listbox.delete(0,tk.END)
+    
+    # global variable - to store selected text's start and end indices
+    selection_indices = []
 
     def show_candidate_event(self):
-    
         # error if no selected words
         self.listbox.delete(0, tk.END)
         print("button pressed")
-
+        self.selection_indices = []
         try:
-            # to get highlight cursor text
-            selection_indices = self.textbox.index(tk.SEL_FIRST), self.textbox.index(tk.SEL_LAST)
-            print("Selection indices:", selection_indices)
-            selected_word = self.textbox.get(selection_indices[0],selection_indices[1])
+            # to get highlight cursor text and its start and end indices
+            self.selection_indices = self.textbox.index(tk.SEL_FIRST), self.textbox.index(tk.SEL_LAST)
+            selected_word = self.textbox.get(self.selection_indices[0],self.selection_indices[1])
 
             #limit to the incorrect word only (added 'clickable' tag)
-            tags_at_cursor = self.textbox.tag_names(selection_indices[0])
+            tags_at_cursor = self.textbox.tag_names(self.selection_indices[0])
+            
             if "clickable" in tags_at_cursor:
+                print("clickable: ",self.textbox.tag_names(self.selection_indices[0]))
                 print("Clickable tag is present at the cursor position")
                 candidate_list = self.find_top_k_nearest_words(selected_word,words.words(),3)
-                for candidate in candidate_list:
-                    self.listbox.insert(tk.END,candidate)
-                
-                
-            self.listbox.bind("<ButtonRelease-1>", self.select_candidate_event)
+                for candidate, distance in candidate_list:
+                    list_label = f"{candidate} (Edit Distance: {distance})"
+                    self.listbox.insert(tk.END,list_label)
+            self.listbox.bind("<Double-1>", self.select_candidate_event)
         except:
             print("No word selected")
         
-            
-    def label_button_frame_event(self, item):
-        print(f"label button frame clicked: {item}")
 
     def find_top_k_nearest_words(self,query_input, words_list, k): 
             distances = [] 
             for word in words_list: 
                 distance = Levenshtein.distance(query_input, word) 
-                distances.append((word, distance)) 
+                distances.append((word, distance))
             sorted_distances = sorted(distances, key=lambda x: x[1]) 
-            return [d[0] for d in sorted_distances[:k]] 
+            print("Distances: ",[d for d in sorted_distances[:k]])
+            return [d for d in sorted_distances[:k]] 
     
         
-    
     def check(self,event):
         # Character Count
         self.lableCount.configure(text = "Character Count: " + str(len(self.textbox.get("1.0", "end-1c"))))
@@ -132,43 +148,3 @@ if __name__ == "__main__":
     app = App()
     app.mainloop()
 
-    
-    
-
-    # def check(self,event):
-    #     spell = SpellChecker()
-    #     content = self.textbox.get("1.0",tk.END) #1.0 the first char, 1.1 the second..
-    #     space_count = content.count(" ")
-        
-    #     if space_count != self.old_spaces:
-    #         self.old_spaces = space_count # old_spaces = 1
-            
-    #         for tag in self.textbox.tag_names():
-    #             self.textbox.tag_delete(tag)
-            
-    #         for word in content.split(" "):
-                
-    #             if re.sub(r'\b\w+\b',"",word.lower()) not in words.words(): # check is character
-    #                 position = content.find(word)
-    #                 self.textbox.tag_add(word, f"1.{position}",f"1.{position+len(word)}")
-    #                 self.textbox.tag_config(word, foreground="red")
-    #                 print("Original word: ", word)
-                    
-    #                 correction_list = spell.correction(word)
-    #                 print("Correction:", correction_list)
-    #                 print(spell.candidates(word))
-    #                 # for correction in correction_list:
-    #                     # self.scrollable_label_button_frame.add_item(correction)
-                        
-
-
-
-
-
-# window.text = ScrolledText(window, font =("Arial",14))
-# window.text.bind("<KeyRelease>",window.check)
-# window.text.pack()
-# window.old_spaces = 0
-# window.mainloop()
-
-# SpellingCheck()
